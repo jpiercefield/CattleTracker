732f3d08edfdfba363071385fed974cee7fc761c
hemlo <hemlocked@outlook.com>
2017-02-19 22:49:02 -0600

    Update
    Older version commited before.

--- python/sql.py
+++ python/sql.py
@@ -1,13 +1,19 @@
-import sqlalchemy
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import exists
+from sqlalchemy.orm.util import has_identity
+from sqlalchemy import exc
+import sys
+
 
 engine = create_engine('mysql://root:Cattle@localhost/cattletrax')
 Base = declarative_base(engine)
 
-class animal(Base):
-	__tablename__ = 'animal'
+
+class feeder(Base):
+	__tablename__ = 'feeder'
 	__table_args__ = {'autoload':True}
 
 def loadSession():
@@ -16,8 +22,10 @@ def loadSession():
 	session = Session()
 	return session
 
-
-if __name__ == "__main__":
+	
 	session = loadSession()
-	res = session.query(animal).all()
-	print (res[1].title)
+	q = session.query(feeder).filter(feeder.ref_id==111111)
+	if session.query(q.exists()).scalar():
+		print("it exists")
+
+	

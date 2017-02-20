b859c9de8d7ccfb5ae455c7dc7e010c7ad9ffd02
hemlo <hemlocked@outlook.com>
2017-02-19 22:44:58 -0600

    python code
    Added a python folder and the beginning script for updating the database
    through the feeder.

---
+++ sql.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql://root:Cattle@localhost/cattletrax')
+Base = declarative_base(engine)
+
+class animal(Base):
+	__tablename__ = 'animal'
+	__table_args__ = {'autoload':True}
+
+def loadSession():
+	metadata = Base.metadata
+	Session = sessionmaker(bind=engine)
+	session = Session()
+	return session
+
+
+if __name__ == "__main__":
+	session = loadSession()
+	res = session.query(animal).all()
+	print res[1].title

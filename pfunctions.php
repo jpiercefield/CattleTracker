327ee4b3eb676a1c02af21ab3d340c750becf8ee
hemlo <hemlocked@outlook.com>
2017-03-16 23:28:02 -0500

    Changed .php back to .html.  Created a test.html page to display data.  Fleshed out the function that retrieves from the DB.  Need to work on error handling.

--- pfunctions.php
+++ pfunctions.php
@@ -2,9 +2,14 @@
 	session_start();
 
 	$type = $_POST['type'];
+	if($type == '24'){
+		getData(1);
+	}else if($type == '7'){
+		getData(7);
+	}
 	//print($type);
 	//foo();
-	get24();
+	//get24();
 
 	function login(){
 		$host="localhost";
@@ -20,14 +25,34 @@
 			print($e->getMessage());
 		}
 	}
-	function get24(){
+	function getData($value){
 		$db = login();
-		$stmt = $db->query('SELECT * FROM feeder');
-		while($row = $stmt->fetch()){
-			echo $row['ref_id'] . "\n";
+
+		try{
+			$stmt = $db->prepare('SELECT * FROM feeder where DATEDIFF(NOW(), last_visit_date) between 0 and :value');
+			$stmt->bindParam(':value', $value, PDO::PARAM_INT);
+			$stmt->execute();
+			//$header = $stmt->fetch(PDO::FETCH_ASSOC);
+/*			Don't believe I need this if we're parsing in jquery
+			foreach ($header as $field => $value){
+				echo json_encode($field);
+			}*/
+			//$stmt = $db->query('SELECT * FROM feeder where DATEDIFF(NOW(), last_visit_date) between 0 and 30');
+			$rows = array();
+			while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+				$rows[] = $row;
+				//echo json_encode(echo $row['ref_id'] ." ". $row['num_visits'] . $row['last_visit_date']."\n");
+				
+			}
+			print json_encode($rows);
+		}catch(PDOException $e){
+			$arr = array('error' => $e->getMessage());
+			//print json_encode($arr);
 		}
+		
 	}
 	function foo(){
 		print("shitbags");
 	}
 ?>
+

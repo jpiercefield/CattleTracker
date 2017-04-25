3e681247d9703225dc70f32625f9a942e50de7cb
hemlo <hemlocked@outlook.com>
2017-04-25 12:21:42 -0500

    Cow edit functionality is complete.  Need to do input validation and some aesthetics.

--- PHP/pfunctions.php
+++ PHP/pfunctions.php
@@ -1,4 +1,5 @@
 <?php
+	//header('Content-type: application/json');
 	session_start();
 
 	$type = $_POST['type'];
@@ -11,6 +12,9 @@
 	}
 	if($type == 'db_search'){
 		search();
+	}
+	if($type=='cow_data') {
+		cow_data();
 	}
 	if($type == 'cow_edit'){
 		cow_edit();
@@ -56,7 +60,7 @@
 		$db = login();
 		try{  //Need to prepare statement dependent upon what values have been entered.  Tag/animal/herd/pasture
 				//can i make everything equal to % value %.  Then, if empty field, I pull all of that field
-			$stmt = $db->prepare('SELECT vit.*, ani.* FROM vitals as vit INNER JOIN animal as ani on vit.animal_id = ani.animal_id WHERE ani.tag_id LIKE :tag AND ani.herd_id LIKE :herd AND ani.animal_id LIKE :animal AND ani.pasture_id LIKE :pasture AND ani.a_type LIKE :a_type');
+			$stmt = $db->prepare('SELECT vit.*, ani.* FROM vitals as vit INNER JOIN animal as ani on vit.vital_id = ani.animal_id WHERE ani.tag_id LIKE :tag AND ani.herd_id LIKE :herd AND ani.animal_id LIKE :animal AND ani.pasture_id LIKE :pasture AND ani.a_type LIKE :a_type');
 			$tag = "%".$_POST['tag_id']."%";
 			$animal = "%".$_POST['animal_id']."%";
 			$herd = "%".$_POST['herd_num']."%";
@@ -80,10 +84,10 @@
 
 	}
 
-	function cow_edit(){
+	function cow_data(){
 		$db=login();
 		try {
-			$stmt = $db->prepare('SELECT cow.cow_id, animal.tag_id, vitals.weight, animal.herd_id, animal.pasture_id, cow.due_date, vitals.DOB, vitals.medical_cond, cow.pregnant FROM cow JOIN animal ON cow.cow_id = animal.animal_id JOIN vitals ON vitals.animal_id = animal.animal_id WHERE animal.animal_id = :tag');
+			$stmt = $db->prepare('SELECT cow.cow_id, animal.tag_id, vitals.weight, animal.herd_id, animal.pasture_id, cow.due_date, vitals.DOB, vitals.medical_cond, cow.pregnant FROM cow JOIN animal ON cow.cow_id = animal.animal_id JOIN vitals ON vitals.vital_id = animal.animal_id WHERE animal.animal_id = :tag');
 			$tag = $_POST['tag_id'];
 			$stmt->bindParam(':tag', $tag);
 			$stmt->execute();
@@ -98,7 +102,47 @@
 			}	
 	}
 
+	function cow_edit(){
+		$db=login();
+		
+		
+		$return = array('message' =>'Failed to update info.');
+		$old_animal_id = $_POST['old_animal_id'];
+		$new_animal_id = $_POST['new_animal_id'];
+		$due_date = date('Y-m-d', strtotime($_POST['due_date']));
+		$tag_id = $_POST['tag_id'];
+		$pregnant = $_POST['pregnant'];
+		$DOB = date('Y-m-d', strtotime($_POST['DOB']));
+		$med_cond = $_POST['med_cond'];
+		$pasture = $_POST['pasture'];
+		$herd = $_POST['herd'];
+		$weight = $_POST['weight'];
 
-//SELECT cow.*, animal.*, vitals.* FROM cow JOIN animal ON cow.cow_id = animal.animal_id JOIN vitals ON vitals.animal_id = animal.animal_id WHERE animal.animal_id LIKE :tag
+		$q1 = 'update vitals set weight = :wei, DOB = :db, medical_cond = :med_con where vital_id = :animal_id';
+		$q2 = 'update cow set due_date = :due_date, pregnant = :pregnant where cow_id = :animal_id';
+		$q3 = 'update animal set tag_id = :tag_id, herd_id = :herd, pasture_id = :pasture where animal_id = :animal_id';
+		$q4 = 'update animal set animal_id = :new_animal_id where animal_id = :old_animal_id';
+
+	try{
+		$stmt = $db->prepare($q1);
+		$stmt->execute(array(":wei" => $weight, ":db" => $DOB, ":med_con" => $med_cond, ":animal_id" => $old_animal_id));
+		$stmt2 = $db->prepare($q2);
+		$stmt2->execute(array(":due_date" => $due_date, ":pregnant" => $pregnant, ":animal_id" => $old_animal_id));
+		$stmt3 = $db->prepare($q3);
+		$stmt3->execute(array(":tag_id" => $tag_id, ":herd"=>$herd, ":pasture"=> $pasture, ":animal_id" => $old_animal_id));
+
+		$stmt4 = $db->prepare($q4);
+		$stmt4->execute(array(":new_animal_id" => $new_animal_id, ":old_animal_id" => $old_animal_id));
+		print json_encode($return);
+	}catch(PDOException $e){
+		//$db->rollback();
+		$return = array('message' => $e->getMessage());
+		print json_encode($return);
+		}				
+
+	}
+
+	
+
 ?>
 
